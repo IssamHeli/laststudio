3c3c032ac42d5bde79ba1a631cf147e0f7bcb1b0
IssamHeli <138814350+IssamHeli@users.noreply.github.com>
2023-10-15 20:24:39 +0100

    all cthange last one

--- javascripte.js
+++ javascripte.js
@@ -244,6 +244,33 @@ else if (window.location.pathname.endsWith('/Gallery.html'))
 }
 else 
 {
+  function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.left >= 0 &&
+      rect.bottom >= 0 &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth) 
+    );
+  }
+  
+  // Function to handle scroll animations
+  function handleScrollAnimations() {
+    var elements = document.querySelectorAll('.animate-scroll');
+    
+    elements.forEach(function(element) {
+      if (isInViewport(element)) {
+        element.classList.add('show');
+      }
+    });
+  }
+  
+  // Event listener for scrolling
+  window.addEventListener('scroll', handleScrollAnimations);
+  
+  // Initial check on page load
+  window.addEventListener('load', handleScrollAnimations);
+
   const imageUrls = [
     'images/firstPhoto.jpg',
     'images/leftImageAbout.jpg',
